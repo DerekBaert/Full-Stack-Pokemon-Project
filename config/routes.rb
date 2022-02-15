42333b7a60730cdb824dab9aec3f0da58a6c43cc
DerekBaert <derek.baert@gmail.com>
2022-02-15 11:33:57 -0600

    Revert "Revert "Added a home index and footer""
    This reverts commit 73915c24847b2bf8e900f93cd62bf7489844058c.

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
 
   get 'types' => 'types#index', as: 'types'
   get 'type/:id' => 'types#show', as: 'type', id: /\d+/
+
+  root to: "home#index"
+
+  resources :pokemon, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
 end
